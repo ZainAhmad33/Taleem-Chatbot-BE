083a5d9d9bfc021b86a2a7f5b7adb06ed90b5717
Zain Ahmad <zainwajid33@gmail.com>
2025-02-06 11:17:14 +0500

    Update ChatController.py

--- Controllers/ChatController.py
+++ Controllers/ChatController.py
@@ -15,7 +15,7 @@ fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
 # injecting service dependency
 chat_service_dependency = Annotated[ChatService, Depends(get_chat_service)]
 
-@router.get("/")
+@router.get("")
 async def read_items(chat_service: chat_service_dependency):
     res = chat_service.chat_history
     return {"res": res}
